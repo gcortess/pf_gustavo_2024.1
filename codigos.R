b2eee35686abe6b5654dbd67888189f7a1264ee2
gcortess <gus.lopescortes@gmail.com>
2024-05-23 22:53:10 -0300

    .

--- codigos.R
+++ codigos.R
@@ -70,6 +70,8 @@ grafico_analise1 <- ggplot(decadaformato, aes(x = decada, y = lancamentos, group
   theme(legend.position = "top")+
   scale_x_continuous(breaks = seq(min(decadaformato$decada), max(decadaformato$decada), by = 10))
 grafico_analise1
+ggsave("graficoanalise1.pdf",plot = grafico_analise1 ,width = 158, height = 93, units = "mm")
+
 
 ### met 2 analise 1
 shapiro.test(decadaformato$lancamentos)
@@ -85,7 +87,7 @@ banco2 <- banco %>%
 
 ### ordenado pela temporada
 
-ggplot(banco2, aes(x=season, y=imdb)) +
+grafico_analise2 <- ggplot(banco2, aes(x=season, y=imdb)) +
   geom_boxplot(fill=c("#A11D21"), width = 0.5) +
   guides(fill=FALSE) +
   stat_summary(fun.y="mean", geom="point", shape=23, size=3, fill="white")+
@@ -93,9 +95,8 @@ ggplot(banco2, aes(x=season, y=imdb)) +
   theme_estat() +
   scale_y_continuous(limits = c(0, 9), breaks = seq(0, 9, by = 2))
 
-
-
-
+grafico_analise2
+ggsave("graficoanalise2.png",plot = grafico_analise2 ,width = 158, height = 93, units = "mm")
 
 banco2_est <- filter(banco2, season == "1")
 sd(banco2_est$imdb, na.rm = T)
@@ -159,9 +160,12 @@ grafico_analise3 <- ggplot(banco3) +
     panel.border = element_blank(),
     axis.line = element_line(colour = "black")
   ) +
-  theme(legend.position = "top")
+  theme(legend.position = "top") +
+  scale_y_continuous(limits = c(0, 80), breaks = seq(0, 80, by = 20))
 
 grafico_analise3
+
+ggsave("graficoanalise3.png",plot = grafico_analise3 ,width = 158, height = 93, units = "mm")
 
 #tabela analise 3
 summary(banco3$setting_terrain)
@@ -181,9 +185,49 @@ resultado_teste
 
 # analise 4
 
-ggplot(banco, aes(x=imdb, y=engagement)) + geom_point(colour="#A11D21", size=3) +
-  labs(x="Nota IMDB", y="Engajamento") +
-  theme_estat()
+grafico_analise4 <- ggplot(banco, aes(x=engagement, y=imdb)) + geom_point(colour="#A11D21", size=3, alpha = 0.4) +
+  labs(x="Engajamento", y="Nota IMDB") +
+  theme_estat() +
+  scale_y_continuous(limits = c(0, 8.3), breaks = seq(2, 8, by = 2))
+
+grafico_analise4
+ggsave("graficoanalise4.png",plot = grafico_analise4 ,width = 158, height = 93, units = "mm")
 
 
 cor.test(banco$imdb, banco$engagement, method = "pearson")
+
+sd(banco$engagement, na.rm = T)
+summary(banco$engagement)
+
+
+#analise 5 
+
+banco5 <- banco %>%
+  mutate(capturou = case_when(
+    caught_fred == "True" ~ "Fred",
+    caught_velma == "True" ~ "Velma",
+    caught_shaggy == "True" ~ "Shaggy",
+    caught_daphnie == "True" ~ "Daphnie",
+    caught_scooby == "True" ~ "Scooby",
+    caught_other == "True" ~ "Outro",
+    TRUE ~ "Ninguém"
+  ))
+
+banco5$capturou <- as.factor(banco5$capturou)
+
+grafico_analise5 <- ggplot(banco5) +
+  aes(x = reorder(capturou, engagement,  FUN = median), y = engagement) +
+  geom_boxplot(fill = c("#A11D21"), width = 0.5) +
+  stat_summary(
+    fun = "mean", geom = "point", shape = 23, size = 3, fill = "white"
+  ) +
+  labs(x = "Quem Capturou", y = "Engajamento") +
+  theme_estat()
+grafico_analise5
+
+ggsave("graficoanalise5.png",plot = grafico_analise5 ,width = 158, height = 93, units = "mm")
+
+banco5_est <- banco5 %>% filter(capturou == "Daphnie")
+sd(banco5_est$engagement, na.rm = T)
+summary(banco5_est$engagement)
+  
